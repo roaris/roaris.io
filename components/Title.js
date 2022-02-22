72d9040efcb2cf98f4a8b145e0726340d74b2e5d
roaris <one0803kyopro@gmail.com>
2022-02-22 22:19:01 +0900

    GitHubとTwitterへのリンクを追加

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,17 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export const Title = () => {
   const H1 = styled.h1`
     display: inline;
     margin-right: 10px;
     vertical-align: middle;
+  `;
+
+  const Main = styled.div`
+    margin-bottom: 10px;
   `;
 
   const Img = styled.img`
@@ -15,12 +21,24 @@ export const Title = () => {
     width: 45px;
   `;
 
+  const Icon = styled.a`
+    margin-right: 8px;
+  `;
+
   return (
     <>
-      <H1>
-        <Link href="/">roaris.io</Link>
-      </H1>
-      <Img src="/icon.png" />
+      <Main>
+        <H1>
+          <Link href="/">roaris.io</Link>
+        </H1>
+        <Img src="/icon.png" />
+      </Main>
+      <Icon href="https://github.com/roaris">
+        <GitHubIcon />
+      </Icon>
+      <Icon href="https://twitter.com/jev_mk">
+        <TwitterIcon />
+      </Icon>
     </>
   );
 };
